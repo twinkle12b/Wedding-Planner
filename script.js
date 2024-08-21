0254ae23ddccbeb253ece80b3820a34f9aefab37
twinkle12b <146715591+twinkle12b@users.noreply.github.com>
2024-08-21 14:32:47 +1000

    Template for wedding website

---
+++ script.js
@@ -0,0 +1,82 @@
+let navbar = document.querySelector('.header .navbar');
+let menuBtn = document.querySelector('#menu');
+
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+};
+document.querySelectorAll('.image-container img').forEach(image => {
+ image.onclick = () => {
+  document.querySelector('.popup-image').style.display = 'block';
+  document.querySelector('.popup-image img').src = image.getAttribute('src');
+}
+});
+
+document.querySelector('.popup-image span').onclick = () => {
+  document.querySelector('.popup-image').style.display = 'none';
+}
+
+
+
+var swiper = new Swiper(".gallery-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop:true,
+    autoplay:{
+      delay: 3000,
+      disableOnInteraction:false,
+    }
+  });
+
+
+var swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+
+  // Pagination bullets
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
+
+
+AOS.init({
+  duration:800,
+  delay:400
+});
